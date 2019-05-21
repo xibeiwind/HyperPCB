09f6a1950de383b99357dd204fe9c7c1faada70e
alex <xibeiwind@126.com>
2019-05-21 09:34:11 +0800

    add ProcessNode

---
+++ HyperPCB.Basic/ProcessNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using HyperPCB.Core;
+using HyperPCB.Core.Enums;
+
+namespace HyperPCB.Basic
+{
+    public class ProcessNode: IProcessNode
+    {
+
+        public Guid Id { get; }
+        public string Name { get; }
+        public ProcessNodeState NodeState { get; }
+        public IProcessNodeContext NodeContext { get; }
+        public IEnumerable<IProcessNodeInputPin> InputPins { get; }
+        public IEnumerable<IProcessNodeOutputPin> OutputPins { get; }
+    }
+}
